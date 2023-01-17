547faba0030d9775efc643bb4670e362f783141c
Nilava Bepari <nilavastnd@gmail.com>
2023-01-17 23:55:50 +0530

    Removed extra code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Navbar } from "./components/Navbar";
-// import { RouteShow } from "./components/RouteShow";
 import { Footer } from "./components/Footer";
 import { Results } from "../src/components/Results";
 
@@ -11,7 +10,6 @@ const App = () => {
 		<div className={darkTheme ? "dark" : ""}>
 			<div className="dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen">
 				<Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
-				{/* <RouteShow /> */}
 				<Results />
 				<Footer />
 			</div>
