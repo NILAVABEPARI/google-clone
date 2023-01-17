ebcb6423f27766fdb962632e58f0be335cde4b4b
Nilava Bepari <nilavastnd@gmail.com>
2023-01-17 23:53:03 +0530

    Name updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
 	return (
 		<div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200 ">
-			© 2022 Goggle Demo, Inc.
+			© 2023 Goggle Clone, Inc.
 		</div>
 	);
 };
